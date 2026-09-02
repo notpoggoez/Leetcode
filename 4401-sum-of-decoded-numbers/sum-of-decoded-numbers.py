55ed5ffa0e455167a37bd0f9edc0282a603c995e
notpoggoez <ericcomputers2021@gmail.com>
2026-09-01 23:36:21 -0400

    Time: 436 ms (52.21%) | Memory: 32.2 MB (51.27%) - LeetSync

---
+++ 4401-sum-of-decoded-numbers/sum-of-decoded-numbers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumDecoded(self, nums: list[int]) -> int:
+        mod = 10**9 + 7
+        count = 0
+
+        for i in range(len(nums)):
+            width = nums[i]%10
+            d = str(nums[i]//10)
+            
+            x = int(d[:width])
+            y = int(d[width:])
+
+            x= x%mod 
+            count = (count + pow(x,y,mod))%mod
+        
+        return count%mod
